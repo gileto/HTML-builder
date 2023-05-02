328ac06c26614b667a278e21bcd948565b1d1858
Maria Gileto <gileto@bk.ru>
2023-05-02 20:02:46 +0400

    feat: done 02-write-filel

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const { stdout, stdin } = process;
+const fs = require("fs");
+const path = require("path");
+
+stdout.write("Привет, пиши свой текст: \n");
+fs.writeFile(path.join(__dirname, "text.txt"), "", (err) => {
+ if (err) throw err;
+});
+let data = "";
+stdin.on("data", (input) => {
+ if (input.includes("exit")) {
+  process.exit();
+ }
+ data = input;
+ fs.appendFile(path.join(__dirname, "text.txt"), data, (err) => {
+  if (err) throw err;
+ });
+});
+
+process.on("SIGINT", () => {
+ stdout.write(" покеда");
+ process.exit();
+});
