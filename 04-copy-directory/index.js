9fa4bce661e36896f90e535529d86d004315d87b
Maria Gileto <gileto@bk.ru>
2023-05-06 18:44:23 +0400

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require("fs/promises");
+const path = require("path");
 
+async function copyDir() {
+ await fs.rm(path.join(__dirname, "files-copy"), {
+  force: true,
+  recursive: true,
+ });
+ await fs.mkdir(path.join(__dirname, "files-copy"));
+ const filesToCopy = await fs.readdir(path.join(__dirname, "files"));
+ for (const file of filesToCopy) {
+  await fs.copyFile(
+   path.join(__dirname, "files", file),
+   path.join(__dirname, "files-copy", file)
+  );
+ }
+}
+
+copyDir().catch((err) => console.error(err));
